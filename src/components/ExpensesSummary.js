7e7a84af95208272ee6e259d5cb446c91fa262c2
nom97 <jaceklasiak@gmail.com>
2018-10-03 13:26:30 +0200

    fetching from firebase

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { connect } from "react-redux";
-import numeral from "numeral";
-import selectExpenses from "../selectors/expenses";
-import selectExpensesTotal from "../selectors/expenses-total";
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
-const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-  const expenseWord = expenseCount === 1 ? "expense" : "expenses";
-  const fromatedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  
   return (
     <div>
-      <h1>
-        Viewing {expenseCount} {expenseWord} totaling {fromatedExpensesTotal}{" "}
-      </h1>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
     </div>
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses, state.filters);
 
   return {
